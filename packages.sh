00413783740c137dec3bc46954d53885cbcb2461
Kevin J. McNamara <kjmcnamara1@gmail.com>
2023-11-28 16:04:06 -0600

    recreated acer.knsv

--- packages.sh
+++ packages.sh
@@ -1,19 +1,28 @@
 # pacman packages
 git
 zsh
-oh-my-zsh
-powerlevel10k
-yay
+vim
+neovim
 python-pip
 python-pipx
 
+oh-my-zsh
+powerlevel10k
+
+# yay
+git clone https://aur.archlinux.org/yay.git /tmp/yay && cd /tmp/yay && makepkg -si
+
 # pipx packages
 konsave
+jupyterlab
+black
 
 # AUR packages
+brave-bin
 emulationstation
 dolphin-emu
 youtube
+visual-studio-code-bin
 
 # other packages
 Fira Code iScript
