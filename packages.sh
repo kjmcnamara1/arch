c215350f70415eaf156d74f08e9ccc7e475847f0
Kevin J. McNamara <kjmcnamara1@gmail.com>
2023-11-28 09:25:01 -0600

    initial commit

---
+++ packages.sh
@@ -0,0 +1,23 @@
+# pacman packages
+git
+zsh
+oh-my-zsh
+powerlevel10k
+yay
+python-pip
+python-pipx
+
+# pipx packages
+konsave
+
+# AUR packages
+emulationstation
+dolphin-emu
+youtube
+
+# other packages
+FiraCode Nerd Font
+touchpad drivers
+megasync
+dolphin-megasync
+profile picture
